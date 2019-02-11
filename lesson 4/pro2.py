3894a61738c43f3bc3f630f8e7af050a52d33768
Kruddyy <43756292+Kruddyy@users.noreply.github.com>
2019-02-11 12:13:06 -0500

    Add files via upload

---
+++ lesson 4/pro2.py
@@ -0,0 +1,4 @@
+for x in range(1000,-1,-1):
+	print(x)
+
+print('Blast off! ')
---
+++ lesson 4/problem1.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+turtle = Turtle()
+
+turtle.color("pink")
+turtle.pensize(5)
+turtle.speed(2)
+turtle.shape("turtle")
+
+
+for y in range(100):
+	# original shape 
+	leo.foward(y>5)
+	for x in range(4):
+		leo.foward(y>2)
+		leo.left(70)
+	leo.backward(35)
+	leo.left(50)
+
+cat = Turtle()
+
+cat.color("purple")
+cat.pensize(2)
+cat.speed(8)
+cat.shape("turtle")
+
+
+
+mainloop()
